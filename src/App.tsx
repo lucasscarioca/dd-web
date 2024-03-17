92a4e4086c55c0c12cde32c2b1066b125aa88315
Lucas Carioca <lucascarioca97@gmail.com>
2024-03-17 17:02:34 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-slate-500 grid place-content-center">
+      <span className="text-slate-100 font-bold">Dino Diary</span>
+      <Button>Test</Button>
+    </div>
+  )
+}
